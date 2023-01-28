2cd1cd21a1182f6e033265ea118851aea8cd1efd
varunsambanni <varunsambanni@gmail.com>
2023-01-28 23:33:22 +0530

    ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,10 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [refresh, setRefresh] = useState(false);
   const [packetType, setPacketType] = useState('*');
-  const [startFlag, setStartFlag] = useState(false);
+
+  if (localStorage.getItem("startFlag") === null) {
+    localStorage.setItem("startFlag", "N");
+  }
 
   const [countsDataAll, setCountsDataAll] = useState([]);
   const [countsDataTCP, setCountsDataTCP] = useState([]);
@@ -17,15 +20,11 @@ function App() {
   const [countsDataIP, setCountsDataIP] = useState([]);
   const [countsDataIPv6, setCountsDataIPv6] = useState([]);
 
-  const startHandler = () => {
-    setStartFlag(true);
-  }
-
   useEffect(() => {
     const interval = setInterval(() => {
       console.log('This will run every second!');
-      console.log(startFlag)
-      if (startFlag == true) {
+      console.log("startFlag ", localStorage.getItem("startFlag"));
+      if (localStorage.getItem("startFlag") == "Y") {
         setRefresh(refresh => !refresh);
       }
       console.log('refresh ', refresh);
@@ -49,6 +48,7 @@ function App() {
           console.log(data.data);
 
           if (packetType === '*') {
+            data.data.reverse();
             setData(data.data);
           }
           else {
@@ -57,6 +57,7 @@ function App() {
                 filteredData.push(data.data[i]);
               }
             }
+            filteredData.reverse();
             setData(filteredData);
           }
         }
@@ -129,59 +130,70 @@ function App() {
     setRefresh(!refresh);
   }
 
+  const startHandler = () => {
+    localStorage.setItem("startFlag", "Y");
+    setRefresh(refresh => !refresh)
+  }
+
+  const stopHandler = () => {
+    localStorage.setItem("startFlag", "N");
+    setRefresh(refresh => !refresh)
+  }
+
 
   return (
     <div className="App">
 
       <div className='toolBar'>
-        <button className='button' onClick={() => startHandler} >SNIFF</button>
-        <button className='button' onClick={() => setStartFlag(false)} >STOP</button>
+        <button className='button' onClick={startHandler} >SNIFF</button>
+        <button className='button' onClick={stopHandler}>STOP</button>
         <button className='button' onClick={clearHandler}>CLEAR</button>
       </div>
 
       <div className='messageContainer'>
-
+        {isLoading ? <div>Loading...</div> : <div>&nbsp;</div>}
+        {localStorage.getItem("startFlag") === "Y" ? <div>Fetching packet data... </div> : <div>Stopped</div>}
       </div>
 
       <div className='graphsContainer'>
         <div className='graph'>
+          <div className='graphLabel'>Packets vs Time</div>
           <LineChart width={280} height={280} data={countsDataAll}>
             <Line type="monotone" dataKey="count" stroke="#8884d8" dots={true} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="time" />
-            <YAxis />
+            <XAxis stroke='white' dataKey="time" />
+            <YAxis stroke='white' />
           </LineChart>
         </div>
         <div className='graph'>
+          <div className='graphLabel'>TCP Packets vs Time</div>
           <LineChart width={280} height={280} data={countsDataTCP}>
             <Line type="monotone" dataKey="count" stroke="#8884d8" dots={true} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="time" />
-            <YAxis />
+            <XAxis stroke='white' dataKey="time" />
+            <YAxis stroke='white' />
           </LineChart>
         </div>
         <div className='graph'>
+          <div className='graphLabel'>UDP Packets vs Time</div>
           <LineChart width={280} height={280} data={countsDataUDP}>
             <Line type="monotone" dataKey="count" stroke="#8884d8" dots={true} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="time" />
-            <YAxis />
+            <XAxis stroke='white' dataKey="time" />
+            <YAxis stroke='white' />
           </LineChart>
         </div>
         <div className='graph'>
+          <div className='graphLabel'>IP Packets vs Time</div>
           <LineChart width={280} height={280} data={countsDataIP}>
             <Line type="monotone" dataKey="count" stroke="#8884d8" dots={true} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="time" />
-            <YAxis />
+            <XAxis stroke='white' dataKey="time" />
+            <YAxis stroke='white' />
           </LineChart>
         </div>
         <div className='graph'>
+          <div className='graphLabel'>IPv6 Packets vs Time</div>
           <LineChart title="graph" width={280} height={280} data={countsDataIPv6}>
             <Line type="monotone" dataKey="count" stroke="#8884d8" dots={true} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="time" />
-            <YAxis />
+            <XAxis stroke='white' dataKey="time" />
+            <YAxis stroke='white' />
           </LineChart>
         </div>
       </div>
@@ -194,37 +206,35 @@ function App() {
           <option value={"IP"}>IP</option>
           <option value={"IPv6"}>IPv6</option>
         </select>
-        {counts.length > 0 &&
-          <div className='counts'>
-            <table>
-              <tr>
-                <th>Packet-Type</th>
-                <th>Count</th>
-              </tr>
-              <tr>
-                <td>TCP</td>
-                <td>{counts[counts.length - 1].counts[0]}</td>
-              </tr>
-              <tr>
-                <td>UDP</td>
-                <td>{counts[counts.length - 1].counts[1]}</td>
-              </tr>
-              <tr>
-                <td>IP</td>
-                <td>{counts[counts.length - 1].counts[2]}</td>
-              </tr>
-              <tr>
-                <td>IPv6</td>
-                <td>{counts[counts.length - 1].counts[3]}</td>
-              </tr>
-            </table>
-          </div>
-        }
+        <div className='counts'>
+          <table>
+            <tr>
+              <th>Packet-Type</th>
+              <th>Count</th>
+            </tr>
+            <tr>
+              <td>TCP</td>
+              <td>{counts.length > 0 ? counts[counts.length - 1].counts[0] : 0}</td>
+            </tr>
+            <tr>
+              <td>UDP</td>
+              <td>{counts.length > 0 ? counts[counts.length - 1].counts[1] : 0}</td>
+            </tr>
+            <tr>
+              <td>IP</td>
+              <td>{counts.length > 0 ? counts[counts.length - 1].counts[2] : 0}</td>
+            </tr>
+            <tr>
+              <td>IPv6</td>
+              <td>{counts.length > 0 ? counts[counts.length - 1].counts[3] : 0}</td>
+            </tr>
+          </table>
+        </div>
       </div>
       <div className='dataContainer'>
         {data.map((packet, index) => {
           return <pre key={index} className='packetContainer'>
-            <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0.6em' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <div className='packetField'>
                 {packet.packet}
               </div>
